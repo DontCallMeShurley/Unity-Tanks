af1d28d27e3dbed140e2275499b595e230b929ca
Dmitrii Vazanov <55343840+lovelasi4ek@users.noreply.github.com>
2021-07-16 17:05:49 +0300

    init

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject exp;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    static int counter = 0;
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+       
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Arena")
+        {
+            Destroy(gameObject);
+            exp = (GameObject) Instantiate(exp, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(exp, 0.2f);
+
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            
+           
+            Destroy(collision.gameObject);
+            exp = (GameObject)Instantiate(exp, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+            Destroy(exp,0.2f);
+            counter++;
+            Debug.Log(counter);
+            if(counter == 4 )
+            {
+                counter = 0;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+          
+        }
+        if (collision.gameObject.tag == "Tank")
+        {
+            Destroy(collision.gameObject);
+            exp = (GameObject)Instantiate(exp, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(exp,0.2f);
+            counter = 0;
+            SceneManager.LoadScene(4);
+        }
+
+    }
+
+   
+
+
+}
---
+++ Assets/scripts/weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+    public Transform player;
+    private float timer ;
+    private float timeBetweenAtack = 0.5f;
+    public float distanceBetweenTanks ;
+  
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Tank").transform;
+
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Vector2.Distance(transform.position, player.position) < distanceBetweenTanks)
+        {
+            if (timer >= timeBetweenAtack)
+            {
+                shoot();
+            }
+        }
+
+    
+
+    }
+
+    public void shoot()
+    {
+        timer = 0;
+        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+}
